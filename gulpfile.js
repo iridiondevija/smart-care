9df3f5efa68d0061aa760deedd685c7ecdf37882
iridiondevija <99794019+iridiondevija@users.noreply.github.com>
2025-09-23 11:00:53 +0200

    fixed the config build

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,25 @@ const { src, dest, series, parallel } = require("gulp");
 const shell = require("gulp-shell");
 const del = require("del");
 
-// Task to run the Fomantic UI build process
-// The 'return' statement is crucial for Gulp to wait for completion
+function clean() {
+  return del(["dist", "build"]);
+}
+
 function semanticBuild() {
   return src(".", { allowEmpty: true }).pipe(
     shell("cd semantic && npx gulp build")
   );
 }
 
-// Task to copy all static assets to a new 'build' folder
-function copyAssets() {
+function copyStaticAssets() {
   return src([
     "**/*",
+    "!semantic", // Exclude the semantic source folder
+    "!semantic/**",
     "!node_modules",
     "!node_modules/**",
-    "!semantic",
-    "!semantic/**",
+    "!dist", // Exclude the new dist folder, it's already built.
+    "!dist/**",
     "!gulpfile.js",
     "!package.json",
     "!package-lock.json",
@@ -25,13 +28,5 @@ function copyAssets() {
   ]).pipe(dest("build"));
 }
 
-// Clean the build directory before creating a new one
-function clean() {
-  return del(["build"]);
-}
-
-// Define the main 'build' task
-exports.build = series(clean, semanticBuild, copyAssets);
-
-// Set 'build' as the default task
+exports.build = series(clean, semanticBuild, copyStaticAssets);
 exports.default = exports.build;
