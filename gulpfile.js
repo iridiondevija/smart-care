9175f8246acb06a703f969724b6d5f4c5399de1d
iridiondevija <99794019+iridiondevija@users.noreply.github.com>
2025-09-23 11:41:45 +0200

    fixed the gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ function semanticBuild() {
 }
 
 // ⚠️ Corrected function ⚠️
+// This function copies the Fomantic UI assets to the correct location.
 function copySemanticAssets() {
   // Copies the entire 'semantic/dist' folder's contents to 'build/dist'
   return src("semantic/dist/**/*").pipe(dest("build/dist"));
